2a737c3bfc0907efc2f2e504f032fc7a171b1d6b
Dong Ying <dongying1989@gmail.com>
2016-11-06 21:31:14 +0800

    Fix the API so that the taken array it returns in the incidents list contains integer as the UserId but not string

--- reducers/User.js
+++ reducers/User.js
@@ -1,4 +1,4 @@
-const fakeUser = {id: "1", name: '老王', phone: '18712345678'};
+const fakeUser = {id: 1, name: '老王', phone: '18712345678'};
 const user = (state = fakeUser, action) => {
     return state;
 };
